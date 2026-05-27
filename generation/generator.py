d010fd76264842d013b0cf7650f9d7dd4eb2b48a
sofa797 <koch.sof.2006@gmail.com>
2026-05-27 14:00:10 +0300

    add prompt in config file and add generator file

---
+++ generation/generator.py
@@ -0,0 +1,40 @@
+import requests
+from utils.config import Config
+
+
+class Generator:
+    def __init__(self):
+        self.url = "https://api.mistral.ai/v1/chat/completions"
+        self.headers = {
+            "Authorization": f"Bearer {Config.MISTRAL_API_KEY}",
+            "Content-Type": "application/json"
+        }
+        self.model = Config.MISTRAL_MODEL
+
+    def build_prompt(self, query, context_chunks):
+        context = "\n\n".join([
+            c["text"] for c in context_chunks
+        ])
+        return Config.PROMPT.format(context=context, query=query)
+    
+    def generate(self, query, context_chunks):
+        prompt = self.build_prompt(query, context_chunks)
+        payload = {
+            "model": self.model,
+            "messages": [
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+            "temperature": Config.TEMPERATURE,
+            "max_tokens": Config.MAX_TOKENS
+        }
+        response = requests.post(
+            self.url,
+            headers=self.headers,
+            json=payload
+        )
+        if response.status_code != 200:
+            return f"error LLM: {response.text}"
+        return response.json()["choices"][0]["message"]["content"]
